f577b23257941eadf2c3a82220dc8db15ed2803c
rodpolako <rod_polako@hotmail.com>
2025-03-06 16:28:23 -0500

    ios

--- ios/assets/chess-pgn-trainer.js
+++ ios/assets/chess-pgn-trainer.js
@@ -1378,7 +1378,7 @@ $(() => {
 	// 		https://stackoverflow.com/questions/47664777/javascript-file-input-onchange-not-working-ios-safari-only
 	$(document).ready(function () {
 		var deviceAgent = navigator.userAgent.toLowerCase();
-		if (deviceAgent.match(/(iphone|ipod|ipad|AppleWebKit)/)) {
+		if (deviceAgent.match(/(iphone|ipod|ipad|applewebkit)/)) {
 			$('#openPGN').attr('accept', '');
 		}
 	});
