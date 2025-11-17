9297388d495357f18f44786eb62260577774eb28
rodpolako <rod_polako@hotmail.com>
2025-11-17 10:39:29 -0500

    fix

--- beta/src/app/config.js
+++ beta/src/app/config.js
@@ -2,7 +2,7 @@ const configuration = {
 	// Application Information
 	app: {
 		name: 'Chess PGN Trainer',
-		version: '1.13.3 Beta',
+		version: '1.13.3',
 	},
 
 	// Application defaults (referenced when the "Reset" button is used)
