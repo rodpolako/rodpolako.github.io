5ac4943208f6e2a7ba97574f2e557566a1edb4f3
rodpolako <rod_polako@hotmail.com>
2025-12-01 10:52:23 -0500

    test7

--- beta/src/util/shared-tools-module.js
+++ beta/src/util/shared-tools-module.js
@@ -72,6 +72,10 @@ function hashJSON(obj) {
  * @returns
  */
 function sort_by_key(array, key, descending = false) {
+	if (array === null) {
+		return;
+	}
+
 	return array.sort(function (a, b) {
 		var x = a[key];
 		var y = b[key];
